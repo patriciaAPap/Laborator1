42357d6d89af108f3cda4fb689d9772915d202cb
patricia.achim <patriciaAPucms@gmail.com>
2023-09-19 02:33:36 +0300

    added Lab1Ex3

--- Lab1Ex3/Program.cs
+++ Lab1Ex3/Program.cs
@@ -9,6 +9,23 @@ namespace Lab1Ex3
     {
         static void Main(string[] args)
         {
+
+            /*Ex3
+          Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastatura:*/
+
+
+            Console.WriteLine("Introudceti un numar intreg din mai multe cifre:");
+
+            int numarIntreg = int.Parse(Console.ReadLine());
+
+            int lastNumber = numarIntreg % 10;
+
+            Console.WriteLine("Ultima cifra a numarului intreg introdus este " + lastNumber + ".");
+
+            /*Aici nu am stiut cum sa rezolv eroarea in caz ca inserez un numar foarte mic sau foarte mare: "Value was either too large or too small for an Int32". 
+             * Cum as fi putut sa fac?*/
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Lab1Ex4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex 4
+Scrieti un program care va afisa semnul unui numar citit de la tastatura
+• Daca numarul este pozitiv, va afisa “+”
+• Daca numarul este negativ, va afisa “-”
+• Daca numarul este 0, va afisa “0”*/
+
+
+            Console.WriteLine("Introudceti numerele pentru media aritmetica:");
+
+            int primulNumar = int.Parse(Console.ReadLine());
+
+            int alDoileaNumar = int.Parse(Console.ReadLine());
+
+            int alTreileaNumar = int.Parse(Console.ReadLine());
+
+            int mediaAritmeticaATreiNumere = (primulNumar + alDoileaNumar + alTreileaNumar) / 3;
+
+
+            Console.WriteLine("Media aritmetica este " + mediaAritmeticaATreiNumere + ".");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
