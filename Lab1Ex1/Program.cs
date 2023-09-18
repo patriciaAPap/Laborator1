f8c8682027a7a145cf2c7a5a971ffb492538d074
patricia.achim <patriciaAPucms@gmail.com>
2023-09-19 02:27:51 +0300

    add

---
+++ Lab1Ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex1
+             * 
+            Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui paralelipiped dreptunghic, va afisa volumul lui*/
+            
+            Console.WriteLine("Introudceti dimensiunile paralelipipedului dreptunghic:");
+            int dimensiune1Paralelipiped = int.Parse(Console.ReadLine());
+
+            int dimensiune2Paralelipiped = int.Parse(Console.ReadLine());
+
+            int dimensiune3Paralelipiped = int.Parse(Console.ReadLine());
+
+            int volumParalelipiped = dimensiune1Paralelipiped * dimensiune2Paralelipiped * dimensiune3Paralelipiped;
+
+
+            Console.WriteLine("Volumul paralelipipedului dreptunghic este " + volumParalelipiped + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1Ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Ex2
+            Scrieti un program care va calcula media aritmetica a trei numere citite de la tastatura*/
+
+
+            Console.WriteLine("Introudceti numerele pentru media aritmetica:");
+
+            int primulNumar = int.Parse(Console.ReadLine());
+
+            int alDoileaNumar = int.Parse(Console.ReadLine());
+
+            int alTreileaNumar = int.Parse(Console.ReadLine());
+
+            int mediaAritmeticaATreiNumere = (primulNumar + alDoileaNumar + alTreileaNumar) / 3;
+
+
+            Console.WriteLine("Media aritmetica este " + mediaAritmeticaATreiNumere + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
