a855e89ec6af4bdd45b53c3a0d7db1bb70c82321
patricia.achim <patriciaAPucms@gmail.com>
2023-09-19 04:45:30 +0300

    updated Lab1Ex9

---
+++ Lab1Ex9/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Ex9          
+Folosind instructiunea “switch”, scrieti un program care citind de la tastatura un numar
+intreg va verifica urmatoarele
+“unu” daca numarul citit este 1
+“doi” daca numarul citit este 2
+“trei” daca numarul citit este 3
+“cinci” daca numarul citit este 5
+“opt” daca numarul citit este 8
+“neidentificat” pentru orice alt caz */
+
+
+
+            Console.WriteLine("Introudceti un numar intreg:");
+
+            int numarulIntrodus = int.Parse(Console.ReadLine());
+
+            switch (numarulIntrodus)
+
+            {
+                case 1:
+                    Console.WriteLine("unu");
+                    break;
+                case 2:
+                    Console.WriteLine("doi");
+                    break;
+                case 3:
+                    Console.WriteLine("trei");
+                    break;
+                case 5:
+                    Console.WriteLine("cinci");
+                    break;
+                case 8:
+                    Console.WriteLine("opt");
+                    break;
+                default:
+                    Console.WriteLine("neidentificat");
+                    break;
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
